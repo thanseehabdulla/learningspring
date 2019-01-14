eb11b2d85776eac1d0e51d693926eae2bb874d11
abdullathanseeh <abdullathanseeh@simelabs.com>
2019-01-14 16:18:44 +0530

    event handler added

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -3,15 +3,32 @@ package com.tutorialspoint;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.*;
 
 public class MainApp {
-   public static void main(String[] args) {
-      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+   private static ApplicationContext context;
+   private static ApplicationContext ctx, ctx2;
+
+
+public static void main(String[] args) {
+      context = new ClassPathXmlApplicationContext("Beans.xml");
+    
+      // Let us raise a start event.
+      ((AbstractApplicationContext) context).start();
+      
+      
+      ctx = new AnnotationConfigApplicationContext(HelloWorldConfig.class);
+//      ctx2 = new AnnotationConfigApplicationContext(TextEditorConfig.class);
+      
+      HelloWorld obj = (HelloWorld) ctx.getBean("helloWorld");
       obj.getMessage();
+      
+//      TextEditor tes = ctx2.getBean(TextEditor.class);
+//      tes.spellCheck();
       
       
       HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+      objB.getMessage();
       objB.setMessage("Thanseeh");
       objB.getMessage();
       
@@ -29,6 +46,9 @@ public class MainApp {
       jc.getAddressSet();
       jc.getAddressMap();
       jc.getAddressProp();
+      
+   // Let us raise a stop event.
+      ((AbstractApplicationContext) context).stop();
       
       ((AbstractApplicationContext) context).registerShutdownHook();
    }
